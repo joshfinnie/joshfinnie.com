e5793b5272da1384e1b54a1115e3ab18945c66c5
Josh Finnie <josh.finnie@trackmaven.com>
2014-09-08 17:29:01 -0400

    cleaning up useless stuff

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,16 +18,6 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = 5
 
 STATIC_PATHS = ['assets']

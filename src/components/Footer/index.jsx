64dde626c9b5ff0159777bbcef0872c66dd904ce
Josh Finnie <josh.finnie@trackmaven.com>
2019-01-02 09:18:35 -0500

    updating the footer component to have dynamic year

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,26 +1,30 @@
 /* eslint-disable react/self-closing-comp */
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import './footer.css';
 
-const Footer = () => (
-  <footer>
-    <div className="container">
-      <div className="row">
-        <div className="col-12 mx-auto">
-          <p className="copyright text-muted">
-            &copy; 2010 - 2018.
-            <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">
-              <img
-                alt="Creative Commons License"
-                src="https://i.creativecommons.org/l/by-nc-sa/3.0/80x15.png"
-                className="cc-img"
-              />
-            </a>
-          </p>
+const Footer = () => {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 mx-auto">
+            <p className="copyright text-muted">
+              &copy; 2010 - {year}.
+              <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">
+                <img
+                  alt="Creative Commons License"
+                  src="https://i.creativecommons.org/l/by-nc-sa/3.0/80x15.png"
+                  className="cc-img"
+                />
+              </a>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  </footer>
-);
+    </footer>
+  );
+};
 
 export default Footer;

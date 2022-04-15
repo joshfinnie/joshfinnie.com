9617e4e85c48ba0f8910e92db7720416fa828944
Josh Finnie <josh@jfin.us>
2022-04-14 20:53:48 -0400

    updating to astro 0.26 in plans for Astro 1.0 (#1417)

--- scripts/optimize_images.js
+++ scripts/optimize_images.js
@@ -4,7 +4,6 @@ import fs from "fs-extra";
 
 let matches = glob.sync(`public/**/*.{png,jpg,jpeg}`);
 const MAX_WIDTH = 1080;
-const QUALITY = 90;
 
 if (process.argv.length > 2) {
   matches = [];
@@ -13,17 +12,11 @@ if (process.argv.length > 2) {
   });
 }
 
-console.log(matches);
-
 Promise.all(
   matches.map(async (match) => {
     const stream = sharp(match);
     const info = await stream.metadata();
-    if (info.width < MAX_WIDTH) {
-      return;
-    }
-    const optimizedName = match.replace(/(\..+)$/, (_, ext) => `-optimized${ext}`);
-    await stream.resize(MAX_WIDTH).jpeg({ quality: QUALITY }).toFile(optimizedName);
-    return fs.rename(optimizedName, match);
+    const optimizedName = match.replace(/(\..+)$/, (_, ext) => `.webp`);
+    await stream.resize(MAX_WIDTH).webp().toFile(optimizedName);
   })
 );

d033d8a90586c92a628469b3becfff2e11b32e09
Josh Finnie <josh@jfin.us>
2022-07-26 20:49:58 -0400

    update to Astrojs 1.0.0-rc.1 (#1444)
    also updated how RSS is handled to use @astrojs/rss
    alse updated other packages.

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,24 @@
+import rss from '@astrojs/rss';
+
+const postImportResult = import.meta.globEager('./blog/**/*.md');
+const posts = Object.values(postImportResult);
+
+export const get = () => {
+
+  try {
+    return rss({
+    title: `Blog | www.joshfinnie.com`,
+    site: import.meta.env.SITE,
+    description: "The personal/professional website of Josh Finnie.",
+    customData: `<language>en-us</language>`,
+    items: posts.map((post) => {
+      return {
+        link: post.url,
+        title: post.frontmatter.title,
+        pubDate: post.frontmatter.date,
+      }})
+    });
+  } catch(e) {
+    console.log(e);
+  }
+}

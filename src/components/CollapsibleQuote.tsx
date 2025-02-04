4b6bebceeccf39701f8f3d657ace0dc993ab1227
Josh Finnie <josh.finnie@peopledatalabs.com>
2025-02-04 11:25:23 -0500

    making component a bit more extendible.

--- src/components/CollapsibleQuote.tsx
+++ src/components/CollapsibleQuote.tsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 
-const CollapsibleQuote = ({ children }: { children: React.ReactNode }) => {
+const CollapsibleQuote = ({
+  label = "Content",
+  children,
+}: {
+  label: string;
+  children: React.ReactNode;
+}): React.ReactElement => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="border-l-4 border-gray-500 pl-4 my-4">
-      <button
-        className="mb-2"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        {isOpen ? <>Hide Quote &#9650;</> : <>Show Quote &#9660;</>}
+      <button className="mb-2" onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <>Hide {label} &#9650;</> : <>Show {label} &#9660;</>}
       </button>
       {isOpen && <>{children}</>}
     </div>
@@ -17,4 +20,3 @@ const CollapsibleQuote = ({ children }: { children: React.ReactNode }) => {
 };
 
 export default CollapsibleQuote;
-

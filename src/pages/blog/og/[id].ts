d6fb690cc18adeb6aaa06bba7e8af0aaac6d8381
Josh Finnie <josh.finnie@peopledatalabs.com>
2026-01-05 10:15:16 -0500

    fixing ogimageroute

--- src/pages/blog/og/[id].ts
+++ src/pages/blog/og/[id].ts
@@ -22,7 +22,7 @@ const pages = Object.fromEntries([
   ],
 ]);
 
-export const { getStaticPaths, GET } = OGImageRoute({
+export const { getStaticPaths, GET } = await OGImageRoute({
   pages,
   param: 'id',
   getImageOptions: (_path, page) => ({

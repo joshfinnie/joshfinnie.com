522d3ca76902b2cf9bc7bdf945b1c0bb03849694
Josh Finnie <josh@jfin.us>
2026-02-16 12:15:10 -0500

    (feat) Moving to Starwind UI, Removing React (#1678)

---
+++ src/components/starwind/button/index.ts
@@ -0,0 +1,7 @@
+import Button, { button } from './Button.astro';
+
+const ButtonVariants = { button };
+
+export { Button, ButtonVariants };
+
+export default Button;
---
+++ src/components/starwind/card/index.ts
@@ -0,0 +1,29 @@
+import Card, { card } from './Card.astro';
+import CardAction, { cardAction } from './CardAction.astro';
+import CardContent, { cardContent } from './CardContent.astro';
+import CardDescription, { cardDescription } from './CardDescription.astro';
+import CardFooter, { cardFooter } from './CardFooter.astro';
+import CardHeader, { cardHeader } from './CardHeader.astro';
+import CardTitle, { cardTitle } from './CardTitle.astro';
+
+const CardVariants = {
+  card,
+  cardAction,
+  cardContent,
+  cardDescription,
+  cardFooter,
+  cardHeader,
+  cardTitle,
+};
+
+export { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, CardVariants };
+
+export default {
+  Root: Card,
+  Header: CardHeader,
+  Footer: CardFooter,
+  Title: CardTitle,
+  Description: CardDescription,
+  Content: CardContent,
+  Action: CardAction,
+};
